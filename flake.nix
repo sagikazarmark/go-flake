c08da2cd101fa6389cfe5c7e9d280f093ea4b84d
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2021-11-14 00:54:33 +0100

    build: initial nix file
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Go tools";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        buildDeps = with pkgs; [ git go gnumake ];
+        devDeps = with pkgs; buildDeps ++ [ golangci-lint gotestsum ];
+
+        goShell = go:
+          pkgs.mkShell {
+            buildInputs = (pkgs.lib.remove pkgs.go devDeps) ++ [ go ];
+          };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = devDeps;
+        };
+      });
+}
