31e4ecf6205868186099fbd8a8cbe7d5557ec242
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2021-11-14 16:13:02 +0100

    add gqlgen
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- flake.nix
+++ flake.nix
@@ -70,5 +70,35 @@
             platforms = platforms.unix;
           };
         };
+
+        packages.gqlgen = pkgs.buildGoModule rec {
+          name = "gqlgen";
+          version = "0.14.0";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "99designs";
+            repo = "gqlgen";
+            rev = "v${version}";
+            sha256 = "sha256-pBvPTLNrDZMyoxtsin+MrUCC3www864TPBrOCFNb2U8=";
+          };
+
+          vendorSha256 = "sha256-YWoe3lQS+0bMfUiNzC+n7qlQbCS8WO+jLuaQk5X0+v8=";
+
+          # doCheck = false;
+          subPackages = [ "." ];
+
+          ldflags = [
+            "-s"
+            "-w"
+            "-X github.com/99designs/gqlgen/graphql.Version=v${version}"
+          ];
+
+          meta = with pkgs.lib; {
+            description = "go generate based graphql server library";
+            homepage = "https://github.com/99designs/gqlgen";
+            license = licenses.mit;
+            platforms = platforms.unix;
+          };
+        };
       });
 }
