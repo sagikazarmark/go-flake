4204b56f834a31da1efca5deee7a2d2726fa140d
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2022-05-09 21:47:08 +0200

    chore: update protoc-gen-kit
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- pkgs/protoc-gen-kit/default.nix
+++ pkgs/protoc-gen-kit/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   name = "protoc-gen-kit";
-  version = "0.3.0";
+  version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "sagikazarmark";
     repo = "protoc-gen-kit";
     rev = "v${version}";
-    sha256 = "sha256-2n/YPUSxGpBYW46/BjJjZpHcrAvD2ck16vNjwGZF2To=";
+    sha256 = "sha256-c1PW39GXZ/ONDSi7xb8eLZm0FG2fIinL+1AKXyhpl/k=";
   };
 
-  vendorSha256 = "sha256-rfvFvFOKUo5fVcGzrPIcGPKVeUlVK2T5Huh+ccNST4c=";
+  vendorSha256 = "sha256-uNhFXLlAk9rWgQMVQw30KMMIhPHdVfX3/8izbOB9pXA=";
 
   proxyVendor = true;
 
