1a32d77792361575b617f7e52f107273ef4ba723
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-04-05 03:56:11 +0200

    update protoc-gen-go-kit
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- pkgs/protoc-gen-go-kit/default.nix
+++ pkgs/protoc-gen-go-kit/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   name = "protoc-gen-kit";
-  version = "0.3.0";
+  version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "sagikazarmark";
     repo = "protoc-gen-go-kit";
     rev = "v${version}";
-    sha256 = "sha256-L1r1BlHDMaW0fg7LTgNtdAbE2AcegiJDeaZJNAVhok4=";
+    sha256 = "sha256-m0TkcWgnUOkA9Ke5P9fyM+nediBdj/WgmNF9mElbkkg=";
   };
 
   vendorSha256 = "sha256-sZJy/d7fzGmCWi1R0zDrAoV8OdqbmcXN54M87vDjyuE=";
